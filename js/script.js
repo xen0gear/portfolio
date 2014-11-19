e825c7286470c1ecc6f437610071e71455b8e854
Andy <yhsiang2@student.gsu.edu>
2014-11-18 21:14:13 -0500

    First binding

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 var myApp = angular.module('app', ['ngRoute']);
+
+myApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/', {
+			//templateUrl must alwasy be expressed relative to the index.html file's location!!!
+			//controller is just an javascript function to handle this view
+			templateUrl: 'partials/partial1.html',
+			controller: 'HomeCtrl'
+			});
+		// when('/plans', {
+		// 	templateUrl: 'views/plans.html',
+		// 	controller: 'PlanCtrl'
+		// 	}).
+		// when('/cart', {
+		// 	templateUrl: 'views/cart.html',
+		// 	//controller: 'CartCtrl'
+		// 	}).
+		// when('/', {
+		// 	templateUrl: 'views/home.html',
+		// 	controller: 'HomeCtrl'
+		// 	}).
+		// otherwise({
+		// 	redirectTo: '/'
+		// 	});
+	}]);
+
+myApp.controller('HomeCtrl', function($scope){
+
+});
